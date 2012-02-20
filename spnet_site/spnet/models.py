686feb10fcab60d74c469d3865dad2f4453f1c34
Christopher Lee <leec@chem.ucla.edu>
2012-02-19 22:21:58 -0800

    revised models to use ManyToManyField and related_name, added Subscriptions

--- spnet_site/spnet/models.py
+++ spnet_site/spnet/models.py
@@ -1,39 +1,62 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User # User model from the Authentication system
 # see https://docs.djangoproject.com/en/1.3/topics/auth/
 
+class TopicGroup(models.Model):
+    name = models.CharField(max_length=200)
+    definition = models.TextField()
+    creator = models.ForeignKey(User)
+    datetime_created = models.DateTimeField(default=datetime.now)
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(User, related_name='subscriptions_in')
+    recommender = models.ForeignKey(User, related_name='subscriptions_out')
+    request_all = models.BooleanField() # get all recs, regardless of topic
+    datetime_created = models.DateTimeField(default=datetime.now)
+
 # see http://www.djangobook.com/en/1.0/chapter12/#cn222
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
-    datetime_joined = models.DateTimeField()
+    datetime_joined = models.DateTimeField(default=datetime.now)
     datetime_last_seen = models.DateTimeField()
-    followers = models.ManyToMany(User)
-    following = models.ManyToMany(User)
+    recommenders = models.ManyToManyField(User, through='Subscription',
+                                     symmetrical=False,
+                                     related_name='subscribers')
+    topic_groups = models.ManyToManyField(TopicGroup, related_name='members')
     
 class Author(models.Model):
     name = models.CharField(max_length=1000)
     email =  models.CharField(max_length=1000) # is there a max length for emails?
-    papers = models.ManyToMany(Author)
+    papers = models.ManyToManyField('Paper', related_name='authors')
     user = models.ForeignKey(User, unique=True) # is this author a user?
+
+class PaperDB(models.Model):
+    name = models.CharField(max_length=1000)
+    url_template = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
+    
     
 class Paper(models.Model):
-    arxiv_identifier = models.CharField(max_length=50) # should check if these are well-defined by ArXiv
-    authors = models.ManyToMany(Author)
+    paper_db = models.ForeignKey(PaperDB, related_name='papers')
+    paper_id = models.CharField(max_length=50) # for generality, make it longer
+    url_data = models.CharField(max_length=1000) # additional data for URL construction
     title = models.CharField(max_length=1000)
-    topic_groups = models.ManyToMany(TopicGroup)
-    submitter = models.ForeignKey(User)
+    topic_groups = models.ManyToManyField(TopicGroup,
+                                          related_name='papers')
+    submitter = models.ForeignKey(User, related_name='submissions')
     #comment_threads
 
 class Recommendation(models.Model):
-    recommender = models.ForeignKey(User)
-    paper = models.ForiegnKey(Paper)
+    recommender = models.ForeignKey(User, related_name='recommendations')
+    paper = models.ForeignKey(Paper, related_name='recommendations')
+    must_read = models.BooleanField() # highest level of rec
+    private = models.BooleanField() # don't display this rec
+    topic_groups = models.ManyToManyField(TopicGroup,
+                                          related_name='recommendations')
     has_review = models.BooleanField()
     review_text = models.TextField()
-    datetime = models.DateTimeField()
+    datetime_created = models.DateTimeField(default=datetime.now)
 
-class TopicGroup(models.Model):
-    name = models.CharField(max_length=200)
-    papers = models.ManyToMany(Paper)
-    users = models.ManyToMany(User)
 
 # Comments: http://codeblogging.net/blogs/1/3/    
