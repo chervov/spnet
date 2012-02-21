c3dc47f9f0a6446274aea82a337c8e588b5a1dba
Christopher Lee <leec@chem.ucla.edu>
2012-02-20 16:11:55 -0800

    added Comment data model, docstrings

--- spnet_site/spnet/models.py
+++ spnet_site/spnet/models.py
@@ -2,17 +2,55 @@ from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User # User model from the Authentication system
 # see https://docs.djangoproject.com/en/1.3/topics/auth/
+from mptt.models import MPTTModel, TreeForeignKey
 
 class TopicGroup(models.Model):
+    '''allows anyone to define a topic:
+    Has mappings to many kinds of data via the following attributes:
+
+    members: all users who are interested in this topic.
+    papers: all papers that claim to address this topic.
+    recommendations: all recommendations addressed to this topic.
+
+    Additional mappings for topic merging:
+    merged_to: list of requests to merge this topic into another.
+    merge_from: list of requests to merge another topic into this one.
+    '''
     name = models.CharField(max_length=200)
     definition = models.TextField()
     creator = models.ForeignKey(User)
     datetime_created = models.DateTimeField(default=datetime.now)
 
+class TopicMerge(models.Model):
+    '''allows anyone to merge one topic into another for their
+    own viewing.
+
+    The system would presumably define a threshold percentage
+    of members requesting a merge that would make that merge the
+    default view.  In that case, a user can explicitly prevent
+    the merge (for his own viewing) by setting the block_merger flag.'''
+    user = models.ForeignKey(User, related_name='merge_requests')
+    merge_from = models.ForeignKey(TopicGroup, related_name='merged_to')
+    merged_to = models.ForeignKey(TopicGroup, related_name='merge_from')
+    block_merger = models.BooleanField(default=False)
+
+SUBSCRIPTION_PRIORITY_CHOICES = (
+    (0, 'High'),
+    (1, 'Medium'),
+    (2, 'Low'),
+)
+
 class Subscription(models.Model):
+    '''adds mappings to User object:
+    User.subscriptions_in lists his subscriptions;
+    User.subscriptions_out lists his subscribers.
+
+    request_all: get all recs, from the recommender regardless of topic.
+    '''
     subscriber = models.ForeignKey(User, related_name='subscriptions_in')
     recommender = models.ForeignKey(User, related_name='subscriptions_out')
-    request_all = models.BooleanField() # get all recs, regardless of topic
+    priority = models.IntegerField(choices=SUBSCRIPTION_PRIORITY_CHOICES)
+    request_all = models.BooleanField()
     datetime_created = models.DateTimeField(default=datetime.now)
 
 # see http://www.djangobook.com/en/1.0/chapter12/#cn222
@@ -48,15 +86,59 @@ class Paper(models.Model):
     #comment_threads
 
 class Recommendation(models.Model):
+    '''recommendation object:
+    Recommends a single paper to one or more topic groups;
+    if no topic specified, send the rec to all subscribers of the
+    recommender.
+    
+    Private rec. will not be displayed on the public interface;
+    it will simply be used for helping to suggest other papers
+    the user is likely to like.
+
+    want_to_read just flags a paper for the users reading list;
+    later s/he can decide whether or not to recommend it to others.
+
+    review_comment attr. gives link to the text of this
+    recommendation, if any.
+    has_review should indicate whether such a review_comment exists.'''
     recommender = models.ForeignKey(User, related_name='recommendations')
     paper = models.ForeignKey(Paper, related_name='recommendations')
     must_read = models.BooleanField() # highest level of rec
+    want_to_read = models.BooleanField() # haven't read this yet...
     private = models.BooleanField() # don't display this rec
     topic_groups = models.ManyToManyField(TopicGroup,
                                           related_name='recommendations')
     has_review = models.BooleanField()
-    review_text = models.TextField()
     datetime_created = models.DateTimeField(default=datetime.now)
 
 
 # Comments: http://codeblogging.net/blogs/1/3/    
+
+class Comment(MPTTModel):
+    '''threaded discussion object:
+    can cite one or more papers
+    can be linked to a recommendation as the text of that rec.
+    can be child of a previous comment, or have any number of
+    child comments.
+
+    The actual comment can be specified in either of two ways:
+
+    comment_text contains the actual text.
+
+    source_url gives URL of externally published comment text,
+    if any.'''
+    papers = models.ManyToManyField(Paper, related_name='comments')
+    title = models.CharField(max_length=1000)
+    author = models.ForeignKey(User, related_name='comments')
+    recommendation = models.ForeignKey(Recommendation, unique=True,
+                                       null=True, blank=True,
+                                       related_name='review_comment')
+    parent = TreeForeignKey('self', null=True, blank=True,
+                            related_name='children')
+    comment_text = models.TextField()
+    source_url = models.URLField(blank=True)
+    datetime_created = models.DateTimeField(default=datetime.now)
+
+    class MPTTMeta:
+        # comments on one level will be ordered by date of creation
+        order_insertion_by=['datetime_created']
